a86a40d50b8a384b0b7b025416759d564fac8d9d
shaanbhatia <81708587+shaanbhatia@users.noreply.github.com>
2022-03-27 18:49:37 -0400

    Create a4_q1.c

---
+++ a4_q1.c
@@ -0,0 +1,27 @@
+// 181527590_###_a04_q1.c.txt
+// Shaan Bhatia - Github ID: @shaanbhatia
+// mir
+// Github URL:
+
+/*///////////////////////////////////////////////////////////////////////
+ / Write a multi-threaded program that implements the banker's algorithm /
+ ///////////////////////////////////////////////////////////////////////*/
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <semaphore.h>
+
+// function to read from file
+
+int main(int argc, char *argv[]) {
+	if (argc < 2) {
+		printf("Input file name missing...exiting with error code -1\n");
+		return -1;
+	}
+	return 0;
+}
