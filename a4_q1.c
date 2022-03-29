5e893252f7ede402745879d7ed90b2214a0a7f44
shaanbhatia <81708587+shaanbhatia@users.noreply.github.com>
2022-03-28 20:55:15 -0400

    Update a4_q1 with read file and arguments

--- a4_q1.c
+++ a4_q1.c
@@ -16,12 +16,53 @@
 #include <time.h>
 #include <semaphore.h>
 
-// function to read from file
+int readFile(char *fileName); // function to read from file
+
+typedef struct thread {
+	char tid[1]; //id of the thread as read from file
+} Thread;
 
 int main(int argc, char *argv[]) {
-	if (argc < 2) {
-		printf("Input file name missing...exiting with error code -1\n");
+	if (argc < 5) {
+		printf("Not enough arguments (need available resource quantities)\n");
+		printf("Exit with error code -1\n");
 		return -1;
 	}
+	printf("Main function working: argc: %d\n", argc);
+	readFile("sample4_in.txt");
+	return 0;
+}
+
+int readFile(char *fileName) {
+
+	FILE *in = fopen(fileName, "r");
+	if (!in) {
+		printf("Error in opening input file. Exiting with error code -1\n");
+		return -1;
+	}
+
+	struct stat st;
+	fstat(fileno(in), &st);
+	char *fileContent = (char*) malloc(((int) st.st_size + 1) * sizeof(char));
+	fileContent[0] = '\0';
+	while (!feof(in)) {
+		char line[100];
+		if (fgets(line, 100, in) != NULL) {
+			strncat(fileContent, line, strlen(line));
+		}
+	}
+	fclose(in);
+
+	int i, j, count;
+	count = 0;
+	for (i = 0; i < 6; i++) {
+		for (j = 0; j < 8; j++) {
+			printf("%c", fileContent[count]);
+			count++;
+		}
+		printf("\n");
+		count++;
+	}
+
 	return 0;
 }
